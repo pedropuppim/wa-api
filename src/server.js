bd6a67ab32cea50fb172c3d35d3fc0c04e498a73
Pedro Puppim <ppuppim@gmail.com>
2026-01-24 15:51:33 -0300

    config production

--- src/server.js
+++ src/server.js
@@ -66,6 +66,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
+      sameSite: 'lax',
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
   })
